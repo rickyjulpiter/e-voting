aaba0e1273ed80754e3a5cf516d792a5be82718e
rickyjulpiter <rickyjulpiter14@gmail.com>
2020-10-16 19:25:10 +0700

    phpmailler added

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,25 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitf2790a55a0dcb880540058afb49694d5
 {
+    public static $prefixLengthsPsr4 = array (
+        'P' => 
+        array (
+            'PHPMailer\\PHPMailer\\' => 20,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'PHPMailer\\PHPMailer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInitf2790a55a0dcb880540058afb49694d5::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitf2790a55a0dcb880540058afb49694d5::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
